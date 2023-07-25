4cd80d846cd1db6feb8f0aa886315506758f6605
KlausMikhaelson <satyamsingh5076@gmail.com>
2023-07-25 18:52:53 +0530

    made the socket connection to send msg only to room which user is connected to working

--- index.js
+++ index.js
@@ -18,11 +18,34 @@ const io = require("socket.io")(server, {
     origin: "*",
   },
 });
+
 io.on("connection", (socket) => {
   console.log("connected to socket id:", socket.id);
 
+  socket.on("joinRoom", (roomName) => {
+    // Leave any previous room before joining the new one
+    if (socket.currentRoom) {
+      socket.leave(socket.currentRoom);
+    }
+
+    // Join the new room
+    socket.join(roomName);
+    socket.currentRoom = roomName;
+    console.log(`Socket ${socket.id} joined room ${roomName}`);
+  });
+
   socket.on("canvas", (data) => {
-    console.log("data received")
-    socket.broadcast.emit("canvas", data);
+    console.log("data received");
+    // Emit the data only to the users in the same room
+    if (socket.currentRoom) {
+      io.to(socket.currentRoom).emit("canvas", data);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    // Make sure to leave the room when the user disconnects
+    if (socket.currentRoom) {
+      socket.leave(socket.currentRoom);
+    }
   });
 });
